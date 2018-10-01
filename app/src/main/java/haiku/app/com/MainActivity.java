228d6f1c9668ce05dc14ad81a52351920297d96b
Monisha Bhatia <monishabhatia@Monishas-MacBook-Air.local>
2018-10-01 08:17:35 +0530

    first commit for MAin Activity

---
+++ app/src/main/java/haiku/app/com/MainActivity.java
@@ -0,0 +1,25 @@
+package haiku.app.com;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @OnClick(R.id.tv_lang_eng)
+    public void onClickEnglish() {
+        startActivity(new Intent(this, EnglishActivity.class));
+    }
+}
